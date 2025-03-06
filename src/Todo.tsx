f3fe969809c30f3d56b6bbd0d5ddab7e6a7c7659
shotaro <850123.a@gmail.com>
2025-03-06 20:15:57 +0900

    型定義を効率的に管理（Omitあり）

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,6 +1,6 @@
 import { TodoType } from "./types/todo";
 
-export const Todo = (props: Pick<TodoType, "userId" | "title" | "completed">) => { // Pickで型から必要なものだけ抜き出している
+export const Todo = (props: Omit<TodoType, "id">) => { // Omitで型から不要なものだけ取り除いている
   const {title, userId, completed = false} = props; // completedは必須ではないのでﾃﾞﾌｫﾙﾄの値を指定
   const completeMark = completed ? "【完】" : "【未】";
   return (
