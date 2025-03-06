5e50709c9f9937778fe7473d08a27d22615297c5
shotaro <850123.a@gmail.com>
2025-03-06 19:49:03 +0900

    取得データの型を定義しバグを防ぐ終了

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App1 from './App1.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App1 />
   </StrictMode>,
 )
