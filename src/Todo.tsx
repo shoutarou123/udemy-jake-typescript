4154bc04d6b824920b188144aa0136d01d756929
shotaro <850123.a@gmail.com>
2025-03-06 20:02:39 +0900

    propsに型を定義しよう終了

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,6 +1,13 @@
-export const Todo = (props) => {
-  const {title, userid} = props;
+type TodoType = {
+  userId: number;
+  title: string;
+  completed?: boolean; // ? を付けることで必須ではなくなる
+}
+
+export const Todo = (props: TodoType) => {
+  const {title, userId, completed = false} = props; // completedは必須ではないのでﾃﾞﾌｫﾙﾄの値を指定
+  const completeMark = completed ? "【完】" : "【未】";
   return (
-    <p>{`${title}(ユーザー:${userid})`}</p>
+    <p>{`${completeMark} ${title}(ユーザー:${userId})`}</p>
   )
 }
