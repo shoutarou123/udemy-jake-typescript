1d9cfc2f75e77903189ada1f19268a99f51f2e55
shotaro <850123.a@gmail.com>
2025-03-06 16:24:23 +0900

    typescript基礎終了

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { Practice1 } from './practices/Practice1'
 import { Practice2 } from './practices/Practice2'
+import { Practice3 } from './practices/Practice3'
+import { Practice4 } from './practices/Practice4'
 
 export default function App() {
   
@@ -9,6 +11,8 @@ export default function App() {
     <>
       <Practice1 />
       <Practice2 />
+      <Practice3 />
+      <Practice4 />
     </>
   )
 }
